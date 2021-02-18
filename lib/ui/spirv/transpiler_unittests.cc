903b7f519640bcf2823aaf03b3d3992530434d7a
Christopher Crawford <chriscraws@gmail.com>
2021-02-18 14:51:36 -0800

    Add empty SPIR-V unit testing file

---
+++ lib/ui/spirv/transpiler_unittests.cc
@@ -0,0 +1,14 @@
+#include "flutter/lib/ui/spirv/transpiler.h"
+
+#include "flutter/testing/testing.h"
+
+namespace flutter {
+namespace testing {
+
+TEST(TranspilerTest, TestRuns) {
+	// nothing here yet :)
+	EXPECT_TRUE(true);
+}
+
+}  // namespace testing
+}  // namespace flutter
